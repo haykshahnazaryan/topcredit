9d21fa7620b9b552cb195b38abf6a9e63aa8e1c3
Safaryan93 <87018504+Safaryan93@users.noreply.github.com>
2021-07-12 17:22:45 +0400

    Update scrypt.js

--- js/scrypt.js
+++ js/scrypt.js
@@ -25,3 +25,11 @@ function initMap() {
 	});
 }
 
+$(document).ready(function() {
+    $('.btn_send_form').click(function() {
+        $('#contactform').attr('action',
+                       'mailto:armen.safs@gmail.com?subject=Jeannette Chambliss Digital Portfolio' +
+                       $('#fname').val() + '&body=' + $('#birthday').val() + '&body=' + $('#phone').val() + '&body=' + $('#money').val() + '&body=' + $('#yes').val() + '&body=' + $('#no').val() + '&body=' + $('#no_two').val() + '&body=' + $('#yes_two').val() + '&body=');
+        $('#contactform').submit();
+    });
+});
