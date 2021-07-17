ee1bd2a8470c96fbfa0a9bf9fd85eab5a6cadbb7
Safaryan93 <87018504+Safaryan93@users.noreply.github.com>
2021-07-17 15:48:27 +0400

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,22 +1,22 @@
 <?php
-if(isset($_POST['phone'])) {
-    echo $_POST['phone'];
-}
-//     if(!isset($_POST['phone'])) {
-//         header('Location: https://topcredit.am/');
-//     } else {
-//         $name = $_POST['fname'];
-//         $birtday = $_POST['birthday'];
-//         $phon = $_POST['phone'];
-//         $money = $_POST['money'];
-//         //echo $phone."<br>".$massege;
+// if(isset($_POST['phone'])) {
+//     echo $_POST['phone'];
+// }
+    if(!isset($_POST['phone'])) {
+        header('Location: https://topcredit.am/');
+    } else {
+        $name = $_POST['fname'];
+        $birtday = $_POST['birthday'];
+        $phon = $_POST['phone'];
+        $money = $_POST['money'];
+        //echo $phone."<br>".$massege;
 
-//         $to='armen.safs@gmail.com';
-//         $subject='тема сообщений';
-//         $massege ="name: ".$name."\r\n birtday: ".$birtday."\r\n phone: ".$phon."\r\n money: ".$money;
+        $to='armen.safs@gmail.com';
+        $subject='тема сообщений';
+        $massege ="name: ".$name."\r\n birtday: ".$birtday."\r\n phone: ".$phon."\r\n money: ".$money;
 
-//         mail ($to, $subject, $massege);
+        mail ($to, $subject, $massege);
 
-//         header('Location: https://topcredit.am/');
-//     }
+        header('Location: https://topcredit.am/');
+    }
 ?>
