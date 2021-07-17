df4df57ed072a05ff9110f675d426013c00f63ec
Safaryan93 <87018504+Safaryan93@users.noreply.github.com>
2021-07-17 14:58:25 +0400

    Update email.php

--- email.php
+++ email.php
@@ -1,25 +1,23 @@
 <?php
-if(!isset($_POST['phone'])) {
-    header('Location: https://topcredit.am/');
-}else{
-    $phon = $_POST['fname'];
-    $phon = $_POST['birthday'];
-    $phon = $_POST['phone'];
-    $phon = $_POST['money'];
-    $phon = $_POST['yes'];
-    $phon = $_POST['no'];
-    $phon = $_POST['yes_two'];
-    $masseg = $_POST['no_two'];
-//echo $phone."<br>".$massege;
+    if(!isset($_POST['phone'])) {
+        header('Location: https://topcredit.am/');
+    } else {
+        $phon = $_POST['fname'];
+        $phon = $_POST['birthday'];
+        $phon = $_POST['phone'];
+        $phon = $_POST['money'];
+        $phon = $_POST['yes'];
+        $phon = $_POST['no'];
+        $phon = $_POST['yes_two'];
+        $masseg = $_POST['no_two'];
+        //echo $phone."<br>".$massege;
 
-    $to='armen.safs@gmail.com';
-    $subject='тема сообщений';
-    $massege ="что интересует: ".$masseg."\r\n тел: ".$phon;
+        $to='armen.safs@gmail.com';
+        $subject='тема сообщений';
+        $massege ="что интересует: ".$masseg."\r\n тел: ".$phon;
 
-    mail ($to, $subject, $massege);
+        mail ($to, $subject, $massege);
 
-    header('Location: https://topcredit.am/');
-}
-
-
+        header('Location: https://topcredit.am/');
+    }
 ?>
