64306d782a4675ba186aa9fdea1b75a3960b6a27
Safaryan93 <87018504+Safaryan93@users.noreply.github.com>
2021-07-17 15:56:54 +0400

    Update mail.php

--- mail.php
+++ mail.php
@@ -41,10 +41,10 @@ $mail = new PHPMailer(true);                                 // Passing `true` e
     //Server settings
     $mail->SMTPDebug = 0;                                     // Enable verbose debug output
     $mail->isSMTP();                                         // Set mailer to use SMTP
-    $mail->Host = 'YOUR_HOST_HERE';                       	// Specify main and backup SMTP servers
+    $mail->Host = 'www.gmail.com';                       	// Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                                // Enable SMTP authentication
-    $mail->Username = 'YOUR_SMTP_USERNAME';               // SMTP username
-    $mail->Password = 'YOUR_SMTP_PASSWORD';                              // SMTP password
+    $mail->Username = 'armen.safs@gmail.com';               // SMTP username
+    $mail->Password = 'Mnac4ban';                              // SMTP password
     $mail->SMTPSecure = 'tls';                          // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                 // TCP port to connect to
 
