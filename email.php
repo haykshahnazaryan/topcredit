03e2273538e4e12fd252f01d94b00c98ea8fc3ea
Safaryan93 <87018504+Safaryan93@users.noreply.github.com>
2021-07-17 14:31:05 +0400

    Update email.php

--- email.php
+++ email.php
@@ -2,11 +2,17 @@
 if(!isset($_POST['phone'])) {
     header('Location: https://topcredit.am/');
 }else{
+    $phon = $_POST['fname'];
+    $phon = $_POST['birthday'];
     $phon = $_POST['phone'];
-    $masseg = $_POST['what'];
+    $phon = $_POST['money'];
+    $phon = $_POST['yes'];
+    $phon = $_POST['no'];
+    $phon = $_POST['yes_two'];
+    $masseg = $_POST['no_two'];
 //echo $phone."<br>".$massege;
 
-    $to='info@armatura-garant.ru';
+    $to='armen.safs@gmail.com';
     $subject='тема сообщений';
     $massege ="что интересует: ".$masseg."\r\n тел: ".$phon;
 
